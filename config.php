4f8b22a5c3fb5d879fee2336252cd761a60ebbae
Aviraj Salunkhe <96661930+avirajsalunkhe@users.noreply.github.com>
2025-08-16 01:22:58 +0530

    Update config.php

--- config.php
+++ config.php
@@ -7,13 +7,13 @@ define('DB_USER', 'root');
 define('DB_PASS', '');
 
 // Google OAuth Configuration
-define('GOOGLE_CLIENT_ID', '706702353134-244dq7k975lb7avp0bl7nh6fpv7fl1ms.apps.googleusercontent.com');
-define('GOOGLE_CLIENT_SECRET', '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');
+define('GOOGLE_CLIENT_ID', 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx');
+define('GOOGLE_CLIENT_SECRET', 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx');
 define('GOOGLE_REDIRECT_URI', 'http://localhost/matoshree/callback.php');
 
 // Google Service Account
 define('GOOGLE_SERVICE_ACCOUNT_FILE', 'C:\xampp\htdocs\matoshree\vendor\fleet-aleph-463811-t6-1e6c20dbae16.json');
-define('GOOGLE_DRIVE_FOLDER_ID', '1NKxRGWjAAGa_fBzeElqFrumMI-sxcrVO'); // Optional: specific folder
+define('GOOGLE_DRIVE_FOLDER_ID', 'xxxxxxxxxxxxxxxxxxxxxxxx'); // Optional: specific folder
 
 // Security
 define('SESSION_SECRET', 'your-random-secret-key');
@@ -25,4 +25,5 @@ try {
 } catch(PDOException $e) {
     die("Connection failed: " . $e->getMessage());
 }
+
 ?>
